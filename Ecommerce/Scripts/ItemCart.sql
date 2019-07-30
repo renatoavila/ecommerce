b88e62e19d285ac7a61f1571000b72c785bab5df
camposystem <camposystem@gmail.com>
2019-07-30 08:34:45 -0300

    script

--- Ecommerce/Scripts/ItemCart.sql
+++ Ecommerce/Scripts/ItemCart.sql
@@ -8,15 +8,12 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[product](
+CREATE TABLE [dbo].ShopCart(
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[key] [uniqueidentifier] NOT NULL,
-	[title] varchar(120) NOT NULL,
-	brand  varchar(120) NOT NULL,
-	model varchar(120) NOT NULL,
-	costprice decimal(10,2)  NOT NULL,
-	saleprice decimal(10,2) NOT NULL
- CONSTRAINT [PK_product] PRIMARY KEY CLUSTERED 
+	[date] Datetime NOT NULL,
+	[state] bit NOT NULL
+ CONSTRAINT [PK_ShopCart] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
